8e451c3ca07412de4ebd8035955967381ed21147
Audrey Kawa <audreykawa@MacBook-Pro-de-Audrey.local>
2019-03-12 16:22:34 +0200

    remove webcam

--- asrt_app/static/js/logic.js
+++ asrt_app/static/js/logic.js
@@ -619,20 +619,7 @@ $(document).ready(function () {
     }
 
 
-    //video handling -----------
-
-    var video = document.querySelector("#videoElement");
-
-    if (navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({ video: true })
-            .then(function (stream) {
-                video.srcObject = stream;
-            })
-            .catch(function (error) {
-                console.log("Something went wrong: " + error);
-            });
-    }
-
+   
 
     //microphone handling -------------
 
